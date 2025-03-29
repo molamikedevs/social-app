c0f07c0cae501b832ba4810d11315cc154611705
molamike <120581758+molamikedevs@users.noreply.github.com>
2025-03-30 03:43:51 +0400

    modify profile

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -94,7 +94,7 @@ const Profile = () => {
 							currentUser.imageUrl || '/assets/icons/profile-placeholder.svg'
 						}
 						alt="profile"
-						className="w-28 h-28 lg:h-36 lg:w-36 rounded-full"
+						className="w-20 h-20 lg:h-36 lg:w-36 rounded-full"
 					/>
 					<div className="flex flex-col flex-1 justify-between md:mt-2">
 						<div className="flex flex-col w-full">
@@ -155,79 +155,84 @@ const Profile = () => {
 					</div>
 				</div>
 			</div>
-
-			<div className="flex max-w-5xl w-full">
+			<div className="flex flex-wrap sm:flex-row max-w-5xl w-full">
 				<Link
 					to={`/profile/${id}`}
-					className={`profile-tab rounded-l-lg ${
+					className={`profile-tab sm:rounded-l-lg ${
 						pathname === `/profile/${id}` && '!bg-dark-3'
 					}`}>
-					<img
-						src={'/assets/icons/posts.svg'}
-						alt="posts"
-						width={20}
-						height={20}
-					/>
-					Posts
+					<div className="flex items-center">
+						<img
+							src={'/assets/icons/posts.svg'}
+							alt="posts"
+							width={20}
+							height={20}
+							className="mr-2"
+						/>
+						<span className="text-sm">Posts</span>
+					</div>
 				</Link>
 				<Link
 					to={`/profile/${id}/followers`}
 					className={`profile-tab ${
 						pathname === `/profile/${id}/followers` && '!bg-dark-3'
 					}`}>
-					<svg
-						className="w-6 h-6 text-primary-500 dark:text-white"
-						aria-hidden="true"
-						xmlns="http://www.w3.org/2000/svg"
-						width="30"
-						height="30"
-						fill="none"
-						viewBox="0 0 24 24">
-						<path
-							stroke="currentColor"
-							strokeLinecap="round"
-							strokeWidth="2"
-							d="M4.5 17H4a1 1 0 0 1-1-1 3 3 0 0 1 3-3h1m0-3.05A2.5 2.5 0 1 1 9 5.5M19.5 17h.5a1 1 0 0 0 1-1 3 3 0 0 0-3-3h-1m0-3.05a2.5 2.5 0 1 0-2-4.45m.5 13.5h-7a1 1 0 0 1-1-1 3 3 0 0 1 3-3h3a3 3 0 0 1 3 3 1 1 0 0 1-1 1Zm-1-9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"
-						/>
-					</svg>
-					Followers
+					<div className="flex items-center">
+						<svg
+							className="w-5 h-5 text-primary-500 dark:text-white mr-2"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24">
+							<path
+								stroke="currentColor"
+								strokeLinecap="round"
+								strokeWidth="2"
+								d="M4.5 17H4a1 1 0 0 1-1-1 3 3 0 0 1 3-3h1m0-3.05A2.5 2.5 0 1 1 9 5.5M19.5 17h.5a1 1 0 0 0 1-1 3 3 0 0 0-3-3h-1m0-3.05a2.5 2.5 0 1 0-2-4.45m.5 13.5h-7a1 1 0 0 1-1-1 3 3 0 0 1 3-3h3a3 3 0 0 1 3 3 1 1 0 0 1-1 1Zm-1-9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"
+							/>
+						</svg>
+						<span className="text-sm">Followers</span>
+					</div>
 				</Link>
 				<Link
 					to={`/profile/${id}/following`}
 					className={`profile-tab ${
 						pathname === `/profile/${id}/following` && '!bg-dark-3'
 					}`}>
-					<svg
-						className="w-6 h-6 text-primary-500 dark:text-white"
-						aria-hidden="true"
-						xmlns="http://www.w3.org/2000/svg"
-						width="30"
-						height="30"
-						fill="none"
-						viewBox="0 0 24 24">
-						<path
-							stroke="currentColor"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth="2"
-							d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-						/>
-					</svg>
-					Following
+					<div className="flex items-center">
+						<svg
+							className="w-5 h-5 text-primary-500 dark:text-white mr-2"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24">
+							<path
+								stroke="currentColor"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+							/>
+						</svg>
+						<span className="text-sm">Following</span>
+					</div>
 				</Link>
 				{currentUser.$id === user.id && (
 					<Link
 						to={`/profile/${id}/liked-posts`}
-						className={`profile-tab rounded-r-lg ${
+						className={`profile-tab sm:rounded-r-lg ${
 							pathname === `/profile/${id}/liked-posts` && '!bg-dark-3'
 						}`}>
-						<img
-							src={'/assets/icons/like.svg'}
-							alt="like"
-							width={20}
-							height={20}
-						/>
-						Liked Posts
+						<div className="flex items-center">
+							<img
+								src={'/assets/icons/like.svg'}
+								alt="like"
+								width={20}
+								height={20}
+								className="mr-2"
+							/>
+							<span className="text-sm">Liked Posts</span>
+						</div>
 					</Link>
 				)}
 			</div>
