238a47a0a3afbde9badf3d71e2ddd2e378688de0
molamike <120581758+molamikedevs@users.noreply.github.com>
2025-03-29 10:09:23 +0400

    fix 404: NOT_FOUND when a user refresh page

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -3,23 +3,23 @@ import Loader from "./Loader"
 import GridPostList from "./GridPostList"
 
 type SearchResultProps = {
-    searchPosts: Models.Document[],
-    isSearchFetching: boolean,
-  
+	searchPosts: { documents: Models.Document[] } | null
+	isSearchFetching: boolean
 }
 
-const SearchResults = ({searchPosts, isSearchFetching}: SearchResultProps) => {
+const SearchResults = ({
+	searchPosts,
+	isSearchFetching,
+}: SearchResultProps) => {
+	if (isSearchFetching) return <Loader />
 
-    if(isSearchFetching) return <Loader />
+	if (searchPosts && searchPosts.documents.length > 0) {
+		return <GridPostList posts={searchPosts.documents} />
+	}
 
-    if (searchPosts && searchPosts.length > 0) {
-			return <GridPostList posts={searchPosts} />
-		}
-
-
-  return (
-    <p className="text-light-4 mt-10 text-center w-full">No result found</p>
-  )
+	return (
+		<p className="text-light-4 mt-10 text-center w-full">No result found</p>
+	)
 }
 
 export default SearchResults
